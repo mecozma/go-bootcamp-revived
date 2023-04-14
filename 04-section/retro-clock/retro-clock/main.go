bc80e071c4e5679fa1b60624dd1501f33dfa6e50
Eduard <mecozma@gmail.com>
2023-04-14 12:24:26 +0100

    Refactored the code.

--- 04-section/retro-clock/retro-clock/main.go
+++ 04-section/retro-clock/retro-clock/main.go
@@ -1,3 +1,44 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+// ---------------------------------------------------------
+// EXERCISE: Refactor
+//
+//	Goal is refactoring the clock project by moving some of its parts to
+//	a new file in the same package.
+//
+//	1. Create a new file: placeholders.go
+//	2. Move the placeholder type to placeholder.go
+//	3. Move all the placeholders (zero to nine and the colon) to placeholder.go
+//	4. Move the digits array to placeholders.go
+//
+// HINT
+//
+//   - placeholders.go file should belong to main package as well
+//
+//     To remember how to do so: Rewatch the "PART I — Packages, Scopes and Importing"
+//     section.
+//
+//   - Short declaration won't work in the package scope.
+//     Remember why by watching: "Short Declaration: Package Scope" lecture
+//
+//   - If you receive the following error and you don't know what to do watch:
+//     "Packages - Learn how to run multiple files" lecture.
+//
+//     # command-line-arguments
+//     undefined: placeholder
+//     undefined: colon
+//
+// EXPECTED OUTPUT
+//
+//	The same output before the refactoring.
+//
+// ---------------------------------------------------------
 package main
 
 import (
@@ -8,98 +49,6 @@ import (
 )
 
 func main() {
-	type placeholder [5]string
-
-	zero := placeholder{
-		"███",
-		"█ █",
-		"█ █",
-		"█ █",
-		"███",
-	}
-
-	one := placeholder{
-		"██ ",
-		" █ ",
-		" █ ",
-		" █ ",
-		"███",
-	}
-
-	two := placeholder{
-		"███",
-		"  █",
-		"███",
-		"█  ",
-		"███",
-	}
-
-	three := placeholder{
-		"███",
-		"  █",
-		"███",
-		"  █",
-		"███",
-	}
-
-	four := placeholder{
-		"█ █",
-		"█ █",
-		"███",
-		"  █",
-		"  █",
-	}
-
-	five := placeholder{
-		"███",
-		"█  ",
-		"███",
-		"  █",
-		"███",
-	}
-
-	six := placeholder{
-		"███",
-		"█  ",
-		"███",
-		"█ █",
-		"███",
-	}
-
-	seven := placeholder{
-		"███",
-		"  █",
-		"  █",
-		"  █",
-		"  █",
-	}
-
-	eight := placeholder{
-		"███",
-		"█ █",
-		"███",
-		"█ █",
-		"███",
-	}
-
-	nine := placeholder{
-		"███",
-		"█ █",
-		"███",
-		"  █",
-		"███",
-	}
-	digits := [...]placeholder{
-		zero, one, two, three, four, five, six, seven, eight, nine,
-	}
-
-	separator := placeholder{
-		"   ",
-		" ░ ",
-		"   ",
-		" ░ ",
-		"   ",
-	}
 
 	for {
 		now := time.Now()
@@ -133,5 +82,4 @@ func main() {
 		fmt.Println()
 		time.Sleep(time.Second)
 	}
-
 }
