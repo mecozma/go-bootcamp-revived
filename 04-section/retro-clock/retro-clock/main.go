48acfd3c17fb1277986a261a20fe8f456a4f5f77
Eduard <mecozma@gmail.com>
2023-04-14 13:43:14 +0100

    The clock is shifting right to left.

--- 04-section/retro-clock/retro-clock/main.go
+++ 04-section/retro-clock/retro-clock/main.go
@@ -7,36 +7,68 @@
 // Follow me on twitter: https://twitter.com/inancgumus
 
 // ---------------------------------------------------------
-// EXERCISE: Refactor
+// EXERCISE: Ticker: Slide the Clock
 //
-//	Goal is refactoring the clock project by moving some of its parts to
-//	a new file in the same package.
+//	Your goal is slide the placeholders every second.
+//	Please run the solution to see it in action.
 //
-//	1. Create a new file: placeholders.go
-//	2. Move the placeholder type to placeholder.go
-//	3. Move all the placeholders (zero to nine and the colon) to placeholder.go
-//	4. Move the digits array to placeholders.go
 //
-// HINT
+//	THIS IS A HARD EXERCISE:
+//	+ It will take you days but it will be worth it.
+//	+ For experienced developers, this can take an hour or so.
 //
-//   - placeholders.go file should belong to main package as well
 //
-//     To remember how to do so: Rewatch the "PART I — Packages, Scopes and Importing"
-//     section.
+//	1. You need to determine the starting and the ending digits to create
+//	   the sliding effect.
 //
-//   - Short declaration won't work in the package scope.
-//     Remember why by watching: "Short Declaration: Package Scope" lecture
 //
-//   - If you receive the following error and you don't know what to do watch:
-//     "Packages - Learn how to run multiple files" lecture.
+//	2. Each second, start from the next placeholder, skip the previous one.
+//	   This means: Only draw the next placeholders.
 //
-//     # command-line-arguments
-//     undefined: placeholder
-//     undefined: colon
+//	   Like this:
+//
+//	   12:40:31
+//	   2:40:31
+//	   40:31
+//	   0:31
+//	   :31
+//	   31
+//	   1
+//
+//
+//	3. After the last placeholder is displayed, fill the lines for the missing
+//	   placeholders, and then start from the first placeholder. Draw it to the
+//	   right part of the screen.
+//
+//	   Like this:
+//
+//	   12:40:31
+//	   2:40:31
+//	   40:31
+//	   0:31
+//	   :31
+//	   31
+//	   1
+//	          1
+//	         12
+//	        12:
+//	       12:4
+//	      12:40
+//	     12:40:
+//	    12:40:3
+//	   12:40:31
+//
+//	   As you can see, you need to draw the clock from the right part of the
+//	   screen, beginning from the first placeholder.
+//
+// HINTS
+//   - You would need to clear the screen inside the loop instead of once.
+//     Otherwise the previous placeholders will be left on the screen.
 //
 // EXPECTED OUTPUT
 //
-//	The same output before the refactoring.
+//	Please run the solution to see it in action. Do not look at the source-code
+//	though.
 //
 // ---------------------------------------------------------
 package main
@@ -50,7 +82,11 @@ import (
 
 func main() {
 
-	for {
+	for shift := 0; ; shift++ {
+
+		screen.Clear()
+		screen.MoveTopLeft()
+
 		now := time.Now()
 		hour := now.Local().Hour()
 		minutes := now.Local().Minute()
@@ -59,24 +95,54 @@ func main() {
 		clock := [...]placeholder{
 			digits[hour/10],
 			digits[hour%10],
-			separator,
+			colon,
 			digits[minutes/10],
 			digits[minutes%10],
-			separator,
+			colon,
 			digits[seconds/10],
 			digits[seconds%10],
 		}
-		if seconds%10 == 0 {
-			clock = alarm
-		}
-		screen.Clear()
-		screen.MoveTopLeft()
+
 		for line := range clock[0] {
-			for i, digit := range clock {
+			l := len(clock)
+			// this sets the beginning and ending of the placeholder positions(indexes).
+			// shift % 1 prevents the indexing error.
+			s, e := shift%l, l
+
+			// to slide the placeholders from the right part of the screen.
+			//
+			// here, we assume that as if the clock's length is double of its length.
+			// this makes things easy to manage: that's why: l*2 is there.
+			//
+			// shift is always increasing, for it's to go beyond the clock's length,
+			// it should be equal or greater than l*2, right (after the remainder of course)?
+			//
+			// so, if the clock goes beyond its length; this code detects that,
+			// and resets the starting position to the first placeholder's index,
+			// and it keeps doing so until the clock is fully displayed again.
+			if shift%(l*2) >= l {
+				s, e = 0, s
+			}
+
+			// print empty lines for the missing place holders.
+			// this creates the effect of moving placeholders from right to left.
+			//
+			// l-e can only be non-zero when the above if statement runs.
+			// otherwise, l-e is always zero, because l == e.
+			//
+			// this is one of the other benefits of assuming the length of the
+			// clock as the double of its length. otherwise, l-e would always be 0.
+			for j := 0; j < l-e; j++ {
+				fmt.Print("     ")
+			}
+
+			// draw the digits starting from 's' to 'e'
+			for i := s; i < e; i++ {
 				next := clock[i][line]
-				if digit == separator && seconds%2 == 0 {
+				if clock[i] == colon && seconds%2 == 0 {
 					next = "   "
 				}
+
 				fmt.Print(next, "  ")
 			}
 			fmt.Println()
