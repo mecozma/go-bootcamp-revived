31c693705c8646da2c309b33a9780d4c03fa09d1
Eduard <mecozma@gmail.com>
2023-04-14 11:18:04 +0100

    Retro clock complete.

--- 04-section/retro-clock/retro-clock/main.go
+++ 04-section/retro-clock/retro-clock/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/inancgumus/screen"
+)
 
 func main() {
 	type placeholder [5]string
@@ -88,10 +93,45 @@ func main() {
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
 
-	for line := range digits[0] {
-		for digit := range digits {
-			fmt.Print(digits[digit][line], "  ")
+	separator := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+
+	for {
+		now := time.Now()
+		hour := now.Local().Hour()
+		minutes := now.Local().Minute()
+		seconds := now.Local().Second()
+
+		clock := [...]placeholder{
+			digits[hour/10],
+			digits[hour%10],
+			separator,
+			digits[minutes/10],
+			digits[minutes%10],
+			separator,
+			digits[seconds/10],
+			digits[seconds%10],
+		}
+
+		screen.Clear()
+		screen.MoveTopLeft()
+		for line := range clock[0] {
+			for i, digit := range clock {
+				next := clock[i][line]
+				if digit == separator && seconds%2 == 0 {
+					next = "   "
+				}
+				fmt.Print(next, "  ")
+			}
+			fmt.Println()
 		}
 		fmt.Println()
+		time.Sleep(time.Second)
 	}
+
 }
